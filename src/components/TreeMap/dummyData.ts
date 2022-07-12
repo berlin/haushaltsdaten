38ed36fee4c18abd494e04d418ef308c1b228d4d
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-07-12 17:41:48 +0200

    chore: provide dummy data in tree map structure

---
+++ src/components/TreeMap/dummyData.ts
@@ -0,0 +1,48 @@
+export const DUMMY_DATA = {
+  name: 'bla',
+  children: [
+    {
+      name: 'blub',
+      children: [
+        { name: 'sfsdfdsf', value: 123 },
+        { name: 'gfhghfh', value: 452 },
+      ],
+    },
+    {
+      name: 'zututzutu',
+      children: [
+        { name: 'asdafd', value: 459 },
+        {
+          name: 'gfhghfh',
+          children: [
+            {
+              name: 'jkdkfhksdfsd',
+              value: 453,
+            },
+            {
+              name: 'mnbmmbmb',
+              value: 38,
+            },
+            {
+              name: 'wertrzrt',
+              value: 100,
+            },
+          ],
+        },
+        { name: 'bnmbnmb', value: 125 },
+        { name: 'jhkztj', value: 70 },
+      ],
+    },
+    {
+      name: 'iououio',
+      children: [
+        { name: 'asds', value: 433 },
+        { name: 'asdasda', value: 25 },
+        { name: 'hgjhjkuh', value: 853 },
+        { name: 'dfgdggd', value: 123 },
+        { name: 'hgjhjkuh', value: 200 },
+        { name: 'dfgdggd', value: 67 },
+      ],
+    },
+  ],
+}
