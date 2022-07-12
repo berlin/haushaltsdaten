e372c753e97dcbf6818110c34b64956121651b35
Lucas Vogel <contact@vogelino.com>
2022-07-12 11:03:29 +0200

    refactor: Remove MainMenu from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { StrictMode, FC } from 'react'
 import { Head } from '@components/Head'
 import '../src/style/global.css'
 import { useMatomo } from '@lib/hooks/useMatomo'
-import { MainMenu } from '@components/MainMenu'
 
 interface PagePropType extends Record<string, unknown> {
   title?: string
@@ -26,10 +25,7 @@ const App: FC<{
   return (
     <StrictMode>
       <Head pageTitle={pageProps.title || ''} />
-      <main className="fixed inset-0 bottom-16 overflow-x-hidden overflow-y-auto">
-        <Component {...pageProps} query={parsedQuery} />
-      </main>
-      <MainMenu />
+      <Component {...pageProps} query={parsedQuery} />
     </StrictMode>
   )
 }
