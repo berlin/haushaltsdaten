c6da72b8f237b6b9ec5c0bab0f3983fca86a84a7
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-07-18 12:36:15 +0200

    refactor: extract components for better reuse

---
+++ src/components/FadeInWrapper/index.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { FC, useEffect, useRef } from 'react'
+
+export const FadeInWrapper: FC = ({ children }) => {
+  const [isVisible, setVisible] = useState(false)
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (!wrapperRef.current) return
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting))
+    })
+    observer.observe(wrapperRef.current)
+  }, [])
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={wrapperRef}
+    >
+      {children}
+    </div>
+  )
+}
