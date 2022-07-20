cf24be0650fc6ec1427dd9d2aff8fe16f5cb3eef
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-07-20 16:42:08 +0200

    feat: make district/type combination sharable

--- pages/share.tsx
+++ pages/share.tsx
@@ -1,71 +1,97 @@
-import snakeCase from 'just-snake-case'
-import { getMainTopicData } from '@lib/requests/getMainTopicData'
+import {
+  getMainTopicData,
+  GetMainTopicDataParamsType,
+  HaushaltsdatenRowType,
+} from '@lib/requests/getMainTopicData'
 import {
   createBaseTree,
   createTreeStructure,
-  TreemapHierarchyType,
 } from '@lib/utils/createTreemapStructure'
 import { TreeMapWithData } from '@components/TreeMap/withData'
-import { ParsedPageQueryType } from '@lib/utils/queryUtil'
+import { mapRawQueryToState, ParsedPageQueryType } from '@lib/utils/queryUtil'
 import { GetServerSideProps } from 'next'
 import { FC } from 'react'
 import useDimensions from 'react-cool-dimensions'
+import { districts } from '@data/districts'
+
+const ALL_DISTRICTS_ID: keyof typeof districts = '01' // -> Alle Bereiche
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const parsedQuery = query ? mapRawQueryToState(query) : {}
+
+  const queriedDistrictId =
+    parsedQuery && parsedQuery.district && !Array.isArray(parsedQuery.district)
+      ? parsedQuery.district
+      : null
+
+  const queriedType =
+    typeof parsedQuery.showExpenses === 'undefined' || parsedQuery.showExpenses
+      ? 'Ausgabetitel'
+      : 'Einnahmetitel'
+
   const data = await getMainTopicData({
-    bereich: 'Hauptverwaltung',
-    titelart: 'Ausgabetitel',
+    bereich:
+      !!queriedDistrictId && queriedDistrictId !== ALL_DISTRICTS_ID
+        ? districts[queriedDistrictId as keyof typeof districts]
+        : undefined,
+    titelart: queriedType,
   })
 
   if (!data) {
     throw new Error('No data found for this request')
   }
 
-  const hierarchyData = {
-    id: 'overview',
-    name: 'Übersicht',
-    children: createTreeStructure(createBaseTree(data)),
-  }
+  // const filteredData = data.filter((row) => {
+  //   if (TEST_PATH.length === 1) {
+  //     return snakeCase(row.hauptfunktions_bezeichnung) === TEST_PATH[0]
+  //   }
+  //   if (TEST_PATH.length === 2) {
+  //     return (
+  //       snakeCase(row.hauptfunktions_bezeichnung) === TEST_PATH[0] &&
+  //       snakeCase(row.oberfunktions_bezeichnung) === TEST_PATH[1]
+  //     )
+  //   }
+  //   if (TEST_PATH.length === 3) {
+  //     return (
+  //       snakeCase(row.hauptfunktions_bezeichnung) === TEST_PATH[0] &&
+  //       snakeCase(row.oberfunktions_bezeichnung) === TEST_PATH[1] &&
+  //       snakeCase(row.funktions_bezeichnung) === TEST_PATH[2]
+  //     )
+  //   }
+  // })
 
   return {
     props: {
-      title: 'Willkommen',
+      title: 'Visualisierung',
       query,
-      hierarchy: hierarchyData,
-      data: data
-        .map((item) => ({
-          id: item.id,
-          title: item.einzelplan_bezeichnung,
-          amount: parseInt(item.betrag, 10),
-          group: item.hauptfunktions_bezeichnung,
-          groupId: snakeCase(item.hauptfunktions_bezeichnung),
-        }))
-        .sort((a, b) => b.amount - a.amount)
-        .slice(0, 100),
+      queriedDistrictId: queriedDistrictId,
+      queriedType: queriedType,
+      rawData: data,
     },
   }
 }
 
-export const Home: FC<{
+export const SharePage: FC<{
   query: Partial<ParsedPageQueryType>
-  hierarchy: TreemapHierarchyType
-  data: {
-    id: string
-    title: string
-    amount: number
-    group: string
-    groupId: string
-  }[]
-}> = ({ hierarchy }) => {
+  queriedDistrictId: keyof typeof districts
+  queriedType: GetMainTopicDataParamsType['titelart']
+  rawData: HaushaltsdatenRowType[]
+}> = ({ rawData, queriedDistrictId, queriedType }) => {
   const { observe, width, height } = useDimensions()
 
   return (
     <>
-      <div className="w-screen h-screen overflow-hidden" ref={observe}>
-        {width && height && (
+      <div className="w-full h-screen overflow-hidden" ref={observe}>
+        {rawData && width && height && (
           <TreeMapWithData
-            hierarchy={hierarchy}
+            district={districts[queriedDistrictId]}
+            type={queriedType}
+            hierarchy={{
+              id: 'overview',
+              name: 'Alle Bereiche',
+              children: createTreeStructure(createBaseTree(rawData)),
+            }}
             width={width}
             height={height}
           />
@@ -75,4 +101,4 @@ export const Home: FC<{
   )
 }
 
-export default Home
+export default SharePage
