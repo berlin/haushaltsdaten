507c17ff6ae95c6e08a4329c81a3553dc5cca2bf
Max Eckert <mbe245@nyu.edu>
2022-07-25 12:58:02 +0200

    fix-content-introandfaq
    adding text to FAQ and changing ordering of Intro page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -14,30 +14,22 @@ export const FaqPage: FC = () => (
     <h1 className="font-bold text-4xl mt-26 mb-6">Fragen und Antworten</h1>
     <h2 className="font-bold text-2xl mb-6 mt-12">Was zeigen die Diagramme?</h2>
     <div className="flex justify-center mt-6">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      Die interaktiven Diagramme in ihrer Ausgangsform zeigen eine Übersicht der
+      kompletten Ausgaben und Einnahmen der Berliner Verwaltung. Durch Eingaben
+      der Nutzer:innen werden weitere Detailebenen angezeigt, wie Bezirk oder
+      Funktionen. Die Diagramme wurden erstellt, um die komplexen Haushaltsdaten
+      nutzer:innenfreundlich darzustellen.
     </div>
     <h2 className="font-bold text-2xl mb-6 mt-12">Wo kommen die Daten her?</h2>
     <div className="flex justify-center mt-6">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      Alle Visualisierungen basieren auf dem öffentlich zugänglichen Datensatz
+      des Doppelhaushaltes 2022/2023, der von der Senatsverwaltung für Finanzen
+      bereitgestellt wird und auf dem Berliner Open Data Portal veröffentlicht
+      ist. Darüber hinaus können Nutzer:innen auf dem Open Data Portal die
+      Haushaltsdaten der vergangenen 10 Jahre einsehen.
     </div>
     <h2 className="font-bold text-2xl mb-6 mt-12">
-      Was ist ein Doppelhaushalt?
+      Was ist ein Doppel- und Nachtragshaushalt?
     </h2>
     <div className="flex justify-center mt-6">
       Anstelle einen neuen Haushalt jedes Jahr zu verabschieden, kann eine Stadt
@@ -46,35 +38,34 @@ export const FaqPage: FC = () => (
       dadurch, dass nicht jedes Jahr eine große Haushaltsdebatte stattfindet.
       Über zwei Jahre können sich allerdings natürlich viele Dinge ändern, daher
       kann das Parlament mit einem Nachtragshaushalt nachsteuern, und so
-      Umverteilungen oder Mehrausgaben beschließen.
+      Umverteilungen oder Mehrausgaben beschließen. Dies war beispielsweise
+      während der Corona-Pandemie nötig, um Mehrausgaben für Pflegepersonal und
+      Unterstützung der Berliner Wirtschaft zu gewährleisten.
     </div>
     <h2 className="font-bold text-2xl mb-6 mt-12">
       Wie setzt sich der Berliner Haushalt zusammen?
     </h2>
     <div className="flex justify-center mt-6">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      Der Haushalt setzt sich aus Einnahmen und Ausgaben zusammen. Alle
+      Einnahmen und Ausgaben sind, wie in einem herkömmlichen Budget oder einer
+      Rechnung, einem Einzelposten zugewiesen, der den Verwendungszweck
+      bestimmt. Im Haushaltsplan sind alle Senatsverwaltungen, Bezirke, sowie
+      zentrale Verfassungsorgane, wie beispielsweise das Abgeordnetenhaus.
     </div>
     <h2 className="font-bold text-2xl mb-6 mt-12">
       Wer entscheidet über die Ausgaben?
     </h2>
     <div className="flex justify-center mt-6">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-      justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-      sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+      Das Land Berlin muss alle voraussichtlichen Einnahmen und Ausgaben eines
+      Jahres in einem Plan ausweisen. Man spricht in diesem Zusammenhang vom
+      „Öffentlichen Haushalt“. Jährlich stellt der Senat, das heißt die
+      Senatsverwaltung für Finanzen, einen Haushaltsplanentwurf mit den
+      jeweiligen Einzelhaushalten der Verwaltungen auf. Die Aufteilung auf
+      weitere Aufgaben (zum Beispiel innere Sicherheit, Wirtschaftsförderung,
+      Kultur) erfolgt im politischen Abstimmungsprozess des Abgeordnetenhauses.
+      Als Teil dieses Abstimmungsprozesses wird der Gesetzesvorschlag in
+      diversen Fachausschüssen debattiert und gegebenfalls angepasst, bevor im
+      Plenum über den Haushalt entschieden wird.
     </div>
   </div>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,15 +26,45 @@ export const HomePage: FC = () => {
       </h1>
 
       <div className="flex justify-center mt-20 mb-16">
-        Der Betrieb unserer Stadt erfordert eine hohe Summe an Ausgaben. Dazu
-        gehören laufende Kosten wie die Gehälter für Polizist*innen,
-        Lehrer*innen und Behördenmitarbeiter*innen, aber auch der Betrieb
-        öffentlicher Gebäude und die Beleuchtung des Straßenraums. Zusätzlich
-        müssen langfristige Investitionen in Einrichtungen und Infrastruktur
-        oder Kapitalprojekte abgedeckt werden. Beispiele hierfür sind der Bau
-        von öffentlichen Schulen, die Instandhaltung von Straßen und die
-        Verbesserung von Parks. Wofür welche Gelder ausgegeben werden, ist im
-        Berliner Haushalt festgelegt.
+        Gehälter für Polizist*innen,Lehrer*innen und Behördenmitarbeiter*innen,
+        aber auch der Betrieb öffentlicher Gebäude und die Beleuchtung des
+        Straßenraums: Der Betrieb unserer Stadt erfordert eine hohe Summe an
+        Ausgaben. Dazu kommen langfristige Investitionen in Einrichtungen und
+        Infrastruktur, wie öffentliche Schulen, die Instandhaltung von Straßen
+        und die Verbesserung von Parks. Für alle anfallenden Kosten und wofür
+        welche Gelder ausgegeben werden, ist im Berliner Haushalt festgelegt.
+      </div>
+
+      <FadeInWrapper>
+        <p className="text-4xl flex justify-center mt-16">
+          76,61 Milliarden Euro
+        </p>
+        <p className="text-gray-500 text-sm flex justify-center mb-12">
+          {' '}
+          stehen Berlin im aktuellen Doppelhaushalt als Gesamtausgaben zur
+          Verfügung
+        </p>
+      </FadeInWrapper>
+
+      <div className="flex justify-center mt-20 mb-16">
+        Pro Jahr stehen der Berliner Politik und Verwaltung also über 35
+        Milliarden Euro zur Umsetzung ihrer Ziele zur Verfügung. Da kann es
+        schonmal schwer werden, den Überblick zu behalten, wofür der Senat und
+        die Bezirke wieviel ihrer Ressourcen aufwenden. Um der Öffentlichkeit
+        die Haushaltsdaten verständlich und transparent zu präsentieren, bietet
+        diese Webseite einen Überblick über die Einnahmen und Ausgaben des
+        Landes für die aktuellen zwei Jahre.
+      </div>
+
+      <div className="font-bold text-brand mt-20 mb-16 text-xl">
+        <ul>
+          <li className="mb-3">
+            <a href="https://haushaltsdaten-staging.odis-berlin.de/faq">
+              {' '}
+              → Fragen und Antworten zum Berliner Haushalt
+            </a>
+          </li>
+        </ul>
       </div>
 
       <h2 className="font-bold text-3xl mt-26">Die Haushaltspläne</h2>
@@ -64,17 +94,6 @@ export const HomePage: FC = () => {
           <GroupedBarChart data={TOTAL_EXPENSES} />
         </div>
       </div>
-
-      <FadeInWrapper>
-        <p className="text-4xl flex justify-center mt-16">
-          76,61 Milliarden Euro
-        </p>
-        <p className="text-gray-500 text-sm flex justify-center mb-12">
-          {' '}
-          stehen Berlin im aktuellen Doppelhaushalt als Gesamtausgaben zur
-          Verfügung
-        </p>
-      </FadeInWrapper>
 
       <h2 className="font-bold text-3xl mt-26">Der Ausgabehaushalt</h2>
       <div className="flex justify-center mt-6">
@@ -120,16 +139,15 @@ Treechart- Image  mit Link
 FAQ Seite*/}
 
       <div className="flex justify-center mt-20">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-        amet.
+        Ein Blick auf Verteilung der Ausgaben nach Hauptfunktionsgruppen macht
+        deutlich, dass Ausgaben für Bildungswesen, soziale Sicherung, und
+        allgemeine Dienste gemeinsam über 70% des gesamten Ausgabenhaushaltes
+        ausmachen. Diese Einteilung und resultierende Darstellung ist allerdings
+        nur ein Weg von vielen, die Ausgaben des Berliner Senats und den
+        Bezirken darzustellen. Mithilfe einer interaktiven Grafik, einer
+        sogenannten Tree-Map, können Sie sich die Ausgaben und Einnahmen des
+        Berliner Haushaltes nach individuellen Merkmalen, wie Bezirk, Funktion
+        oder Ämtern anzeigen lassen.
       </div>
 
       <div className="container flex justify-between mt-20">
@@ -175,16 +193,48 @@ FAQ Seite*/}
               → Alle Daten in der Tree Map erkunden
             </a>
           </li>
+        </ul>
+      </div>
+
+      <h2 className="font-bold text-3xl mt-26">
+        Schwerpunktthema: Gender Budgeting
+      </h2>
+      <div className="container flex justify-center mt-6">
+        <div className="flex-col mr-12">
+          Berlin ist bis heute eines der wenigen Bundesländer in Deutschland,
+          das die Umsetzung von Gender Budgeting im Landeshaushalt explizit
+          formuliert hat und inzwischen dabei ist, Gender Budgeting in die
+          normale Haushaltspolitik zu integrieren. Beispiele für die
+          Implementierung von Gender Budget-Ansätzen gibt es in verschiedenen
+          europäischen und außereuropäischen Ländern: Schweiz, Spanien,
+          Österreich, Großbritannien, Australien, Südafrika, Brasilien etc. Die
+          in der Berliner Verwaltung 2003/2004 eingeführte Gender
+          Budget-Nutzenanalyse untersucht überwiegend die Ausgabenseite des
+          Berliner Haushalts, um zu ermitteln, ob die
+          Programme/Angebote/Leistungen vermehrt von Frauen oder Männern in
+          Anspruch genommen werden.
+          <br></br>
+          Auf der Ebene der Hauptverwaltung, also den Senatsverwaltungen,
+          geschieht dies vor allem bei Titeln der Hauptgruppe 6 (Zuweisungen und
+          Zuschüsse ohne Investitionen), hierzu gehören Unterstützungen für
+          natürliche Personen wie auch für Institutionen, z. B. Vereine,
+          Kultureinrichtungen usw. In den Kapiteln der Hauptverwaltung mit
+          Personal werden die Durchschnittsgehälter der Beschäftigten nach
+          Geschlecht ausgewiesen und eventuelle Unterschiede begründet.
+          <br></br>
+          Auf der Ebene der Bezirksverwaltung werden mittlerweile 116
+          Bezirksprodukte unter Gender-Aspekten untersucht, wie beispielsweise
+          die Nutzung von Sportflächen, Musikschulunterricht,
+          Volkshochschulkurse.
+        </div>
+      </div>
+
+      <div className="font-bold text-brand mt-20 mb-16 text-xl">
+        <ul>
           <li className="mb-3">
             <a href="https://haushaltsdaten-staging.odis-berlin.de/schwerpunkte">
               {' '}
               → Ausgewählte Schwerpunktthemen ansehen
-            </a>
-          </li>
-          <li className="mb-3">
-            <a href="https://haushaltsdaten-staging.odis-berlin.de/faq">
-              {' '}
-              → Fragen und Antworten zum Berliner Haushalt
             </a>
           </li>
         </ul>
