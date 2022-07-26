adad7c5d5b04f4ab2922732fc76c5708540cb3d3
Lisa-Stubert <lisastubert@web.de>
2022-07-26 23:17:47 +0200

    style(intro): restyle intro page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import TypeAnimation from 'react-type-animation'
 import { FUNCTION_GROUPS } from '@data/functionGroups'
 import { TOTAL_EXPENSES } from '@data/totalExpenses'
 import { InternalLink } from '@components/InternalLink'
+import { Building } from '@components/Icons'
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getStaticProps: GetStaticProps = async () => {
@@ -19,85 +20,131 @@ export const getStaticProps: GetStaticProps = async () => {
 export const HomePage: FC = () => {
   return (
     <div className="px-8">
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mt-20 flex justify-center">
-        Berliner Haushaltsdaten
-      </h1>
-      <h1 className="text-2xl md:text-4xl text-center flex justify-center ">
-        2022
-      </h1>
 
-      <div className="md:w-3/5 md:text-xl m-auto italic text-center mt-12 md:mt-20 text-brand">
-        Gehälter für Polizist:innen, Lehrer:innen und Behördenmitarbeiter:innen,
-        der Betrieb öffentlicher Gebäude, die Beleuchtung des Straßenraums...
-        Das Leben und Zusammenleben in Berlin verursacht eine hohe Summe an
-        Ausgaben, die durch die Stadtverwaltung getragen werden müssen. Dazu
-        kommen langfristige Investitionen in Einrichtungen und Infrastruktur,
-        wie öffentliche Schulen, die Instandhaltung von Straßen und die
-        Verbesserung von Parks. Wofür wieviele Gelder ausgegeben werden, ist im
-        Berliner Haushalt festgelegt.
-      </div>
+      <div className="md:w-4/5 m-auto mt-12 lg:mt-20">
+        <div className="flex flex-wrap justify-between">
+          <div className="flex-col lg:w-1/2 lg:pr-12">
+            <span className="flex justify-center mt-10 md:mt-20 ">
+              {/* <Building className="fill-gray-400"/> */}
+            </span>
+            <h1 className="text-4xl md:text-6xl lg:text-6xl font-bold lg:text-right mb-3">
+              Berliner <br></br>Haushaltsdaten
+            </h1>
+            <h1 className="text-2xl md:text-4xl md:text-right">
+              2022/23
+            </h1>
+          </div>
+          <div className="flex-col italic lg:not-italic lg:w-1/2 mt-6 md:mt-12 lg:mt-24 lg:pr-28">
+            Pro Jahr stehen der Berliner Politik und Verwaltung über 35 Milliarden
+            Euro zur Umsetzung ihrer Ziele zur Verfügung. Da kann es schonmal
+            schwer werden, den Überblick zu behalten, wofür der Senat und die
+            Bezirke wieviel ihrer Ressourcen aufwenden. Um der Öffentlichkeit die
+            Haushaltsdaten verständlich und transparent zu präsentieren, bietet
+            diese Webseite einen Überblick über die Einnahmen und Ausgaben des
+            Landes für den aktuellen Doppelhaushalt.
+          </div>
+        </div>
+      
 
-      <div className="md:w-3/5 m-auto mt-12 md:mt-20">
-        <div className="flex justify-center">
-          Pro Jahr stehen der Berliner Politik und Verwaltung über 35 Milliarden
-          Euro zur Umsetzung ihrer Ziele zur Verfügung. Da kann es schonmal
-          schwer werden, den Überblick zu behalten, wofür der Senat und die
-          Bezirke wieviel ihrer Ressourcen aufwenden. Um der Öffentlichkeit die
-          Haushaltsdaten verständlich und transparent zu präsentieren, bietet
-          diese Webseite einen Überblick über die Einnahmen und Ausgaben des
-          Landes für die aktuellen zwei Jahre. <br></br>
-        </div>
-        <h2 className="font-bold text-2xl md:text-3xl mt-12 md:mt-20">
-          Einnahmen und Ausgaben
-        </h2>
-        <div className="flex-col mt-6">
-          Das Land Berlin muss alle voraussichtlichen Einnahmen und Ausgaben
-          eines Jahres in einem Plan ausweisen, dem „Öffentlichen Haushalt“.
-          Jährlich stellt der Senat, genauer die Senatsverwaltung für Finanzen,
-          einen Haushaltsplanentwurf mit den jeweiligen Einzelhaushalten der
-          Verwaltungen auf. Haushaltsrechtlich möglich sind auch Haushalte für
-          zwei Jahre, jeweils nach Jahren getrennt. Der Senat macht von der
-          Möglichkeit Gebrauch, solche &quot;Doppelhaushalte&quot; vorzulegen,
-          weil dies den Verwaltungsaufwand der Aufstellung reduziert.
-        </div>
-        <FadeInWrapper>
-          <p className="text-2xl md:text-4xl text-center mt-6 md:mt-16">
-            76,61 Milliarden Euro
-          </p>
-          <p className="text-gray-500 text-xs md:text-sm text-center">
-            {' '}
-            stehen Berlin im aktuellen Doppelhaushalt als Gesamtausgaben zur
-            Verfügung
-          </p>
-        </FadeInWrapper>
-
-        <div className="mt-6 md:mt-16">
-          Die Haushaltslage des Landes hat sich in den vergangenen Jahren stetig
-          verbessert, sodass Berlin seit dem Jahr 2012 zur Finanzierung seiner
-          laufenden Ausgaben keine zusätzlichen Kredite mehr aufnehmen musste.
-        </div>
-        <p className="mt-6 md:mt-16 text-sm flex justify-center">
-          Gesamtausgaben des Berliner Haushalts in Mrd. €
-        </p>
-        <div className="mt-2 flex justify-center">
-          <GroupedBarChart data={TOTAL_EXPENSES} />
+        <div className="lg:w-3/6 m-auto mt-12 md:mt-20">
+          <div className="text-2xl md:text-4xl flex-col">
+            <h1 className="flex lg:mt-28">Wie viel Geld gibt Berlin aus für</h1>
+            <h1 className="font-bold flex text-brand">
+              <TypeAnimation
+                cursor={false}
+                sequence={[
+                  'die Polizei?',
+                  2000,
+                  'öffentliche Museen?',
+                  2000,
+                  'Wohnungsbau?',
+                  2000,
+                  'Straßenbeleuchtung?',
+                  2000,
+                  'Grundsicherung?',
+                  2000,
+                ]}
+                wrapper={'p'}
+                repeat={Infinity}
+              /><span className="text-white"> .</span></h1>        
+          </div>
+          <div className="m-auto mt-6 md:mt-8">
+            Gehälter für Polizist:innen, der Betrieb öffentlicher Gebäude, die Förderungen von kulturellen Einrichtungen, die Beleuchtung des Straßenraums: Das Leben und Zusammenleben in Berlin verursacht viele laufende Kosten. Dazu kommen langfristige Investitionen in Einrichtungen und Infrastruktur, wie öffentliche Schulen und die Verbesserung von Parks. Ausgaben die durch die Stadtverwaltung getragen werden müssen. Doch wofür werden wieviele Gelder ausgegeben?
+            <br></br><br></br>
+            Die Ausgaben sind im sogenannten Berliner Haushalt festgelegt. Sie lassen sich in 9 bundesweit vereinheitlichte Hauptfunktionsbereiche untergliedern. Diese können als oberste Stufe einer Treemap dargestellt werden:
+          </div>   
         </div>
 
-        <div className="font-bold text-brand mt-12 md:mt-20 text-xl">
-          <ul>
-            <li>
-              <InternalLink href={'/faq'}>
-                {'→ Mehr erfahren: Fragen und Antworten zum Berliner Haushalt'}
-              </InternalLink>
-            </li>
-          </ul>
+        <div className="md:flex justify-center m-auto mt-6 md:mt-12 ">
+          <div className="flex-col mb-6 md:my-auto inline-block">
+          <FadeInWrapper>
+            <div className="md:pr-12">
+            <ul>
+            <span className="font-bold text-xl text-brand">
+                <InternalLink href={'/visualisierung'}>
+                  {'→ Zur Visualisierung'}
+                </InternalLink>
+              </span>
+              <li><p className="pl-6">Alle Ausgaben und Einnahmen<br></br>in der Tree Map erkunden</p>
+              </li>
+            </ul>
+          </div>
+          </FadeInWrapper>
+          </div>
+          <div className="flex-col inline-block my-auto justify-center md:pr-10">
+            <iframe style={{ width: "100%" }}
+              title="Beispiel-Visualisierung der Haushaltsdaten"
+              width="400rem"
+              height="400"
+              src="https://haushaltsdaten-staging.odis-berlin.de/share"
+            ></iframe>
+          </div>
         </div>
 
-        <h2 className="font-bold text-2xl md:text-3xl mt-12 md:mt-20">
-          Der Ausgabehaushalt
-        </h2>
-        <div className="flex justify-center mt-6">
-          Im Ausgabehaushalt ist festgelegt, wie viel Geld in den einzelnen
+        <div className="lg:w-3/6 m-auto mt-6 md:mt-12">
+            Im Berliner Haushaltsplan sind die Ausgaben und Einnahmen außerdem spezifischen Bereichen zugeordnet: Der Hauptverwaltung und den zwölf Berliner Bezirken. Außerdem werden die Beträge für den einzelnen Haushaltsjahren getrennt angegeben. Mittels der Visualisierung die über den Link oben erreichbar ist, können die einzelnen Bereiche und Jahre erkundet werden.
+            <br></br><br></br>
+            Die Hauptfunktionen lassen sich in weitere Funktionsbereiche untergliedern. Die unterste detailierteste Angabe zu spezifischen Ausgaben und Einnahmen sind die sogenannten Titelbezeichung. Sie können über die Visualisierung angezeigt werden und erscheinen dann unter der Tree Map. Titel und Funktionen können aber auch gezielt über die Suchfunktion gefunden werden.  
+          <div className="flex justify-center mt-6 md:mt-12">
+            <FadeInWrapper>
+              <ul>
+              <span className="font-bold text-xl text-brand">
+                  <InternalLink href={'/suche'}>
+                    {'→ Zur Suche'}
+                  </InternalLink>
+                </span>
+                <li><p className="pl-6">Nach Stichwörtern in den Einnahmen und Ausgaben suchen</p>
+                </li>
+              </ul>
+            </FadeInWrapper>
+          </div>
+        </div>
+
+        <div className="lg:w-3/6 m-auto mt-6 md:mt-12">
+          <h2 className="text-2xl md:text-3xl mt-12 md:mt-20">
+            Wissenwertes zum Berliner Haushalt
+          </h2>
+          <div className="flex-col mt-6">
+            Wie genau funktioniert das eigentlich mit dem Haushalt? Das Land Berlin muss alle voraussichtlichen Einnahmen und Ausgaben
+            eines Jahres in einem Plan ausweisen, dem „Öffentlichen Haushalt“.
+            Jährlich stellt der Senat, genauer die Senatsverwaltung für Finanzen,
+            einen Haushaltsplanentwurf mit den jeweiligen Einzelhaushalten der
+            Verwaltungen auf. Haushaltsrechtlich möglich ist es auch Haushalte direkt für
+            zwei Jahre, jeweils nach Jahren getrennt, festzusetzen. In Berliner macht man von dieser Regelung gebrauch, solche &quot;Doppelhaushalte&quot; vorzulegen,
+            da dadurch der Verwaltungsaufwand reduziert wird.
+          </div>
+
+            <p className="text-2xl text-center mt-6 md:mt-16">
+              76,61 Milliarden Euro
+            </p>
+            <p className="text-gray-500 text-xs md:text-sm text-center">
+              {' '}
+              stehen Berlin im aktuellen Doppelhaushalt als Gesamtausgaben zur
+              Verfügung
+            </p>
+
+          <div className="mt-6 md:mt-16">
+            Im Ausgabehaushalt ist festgelegt, wie viel Geld in den einzelnen
           Politikbereichen ausgegeben werden darf. Gleichzeitig wird damit das
           Budget (Etat) für die Erfüllung der öffentlichen Aufgaben Berlins
           jährlich festgeschrieben. Die zu erwartenden Einnahmen aus Steuern und
@@ -108,135 +155,53 @@ export const HomePage: FC = () => {
           Aufteilung auf die weiteren Aufgaben der Verwaltung (z.B. innere
           Sicherheit, Wirtschaftsförderung, Kultur) erfolgt im politischen
           Abstimmungsprozess des Abgeordnetenhauses.
-        </div>
-        <FadeInWrapper>
-          <p className="mt-6 text-sm flex justify-center">
-            Verteilung der Ausgaben in den 10 Hauptfunktionsgruppen im
-            Doppelhaushalt 2022/23
-          </p>
-        </FadeInWrapper>
-        <div className="mt-2 flex justify-center">
-          <div>
-            {FUNCTION_GROUPS.map((functionGroup) => (
-              <FadeInWrapper key={functionGroup.name}>
-                <div
-                  className="box flex justify-between"
-                  style={{
-                    backgroundColor: functionGroup.color,
-                    height: functionGroup.height + 'rem',
-                  }}
-                >
-                  <span>{functionGroup.name}</span>
-                  <span>{functionGroup.value}</span>
-                </div>
-              </FadeInWrapper>
-            ))}
+          </div>
+
+          <p className="mt-6 md:mt-16 text-sm flex justify-center">
+            Gesamtausgaben des Berliner Haushalts in Mrd. €
+         </p>
+          <div className="mt-2 flex justify-center">
+            <GroupedBarChart data={TOTAL_EXPENSES}/>
+          </div>
+          <div className="mt-6 md:mt-16">
+            Die Haushaltslage des Landes hat sich in den vergangenen Jahren stetig
+            verbessert, sodass Berlin seit dem Jahr 2012 zur Finanzierung seiner
+            laufenden Ausgaben keine zusätzlichen Kredite mehr aufnehmen musste.
+          </div>
+          <div className="flex justify-center mt-6 md:mt-12">
+            <FadeInWrapper>
+              <ul>
+              <span className="font-bold text-xl text-brand">
+                  <InternalLink href={'/faq'}>
+                    {'→ Zu weiteren Infos'}
+                  </InternalLink>
+                </span>
+                <li><p className="pl-6">Mehr erfahren in den Fragen und Antworten zum Berliner Haushalt</p>
+                </li>
+              </ul>
+            </FadeInWrapper>
           </div>
         </div>
 
-        {/* Piechart oder Sankey Bezirke Hauptverwaltungen 
-  Treechart- Image  mit Link
-  FAQ Seite*/}
-
-        <div className="flex justify-center mt-12 md:mt-2016">
-          Ein Blick auf Verteilung der Ausgaben nach Hauptfunktionsgruppen macht
-          deutlich, dass Ausgaben für Bildungswesen, soziale Sicherung, und
-          allgemeine Dienste gemeinsam über 70% des gesamten Ausgabenhaushaltes
-          ausmachen. Diese Einteilung und resultierende Darstellung ist
-          allerdings nur ein Weg von vielen, die Ausgaben des Berliner Senats
-          und den Bezirken darzustellen. Mithilfe einer interaktiven Grafik,
-          einer sogenannten Tree-Map, können Sie sich die Ausgaben und Einnahmen
-          des Berliner Haushaltes nach individuellen Merkmalen, wie Bezirk,
-          Funktion oder Ämtern anzeigen lassen.
-        </div>
-
-        <div className="flex flex-wrap justify-between mt-6 md:mt-20">
-          <div className="text-2xl md:text-4xl flex-col">
-            <h1 className="flex lg:mt-28">Wie viel Geld wird für</h1>
-            <h1 className="font-bold flex text-brand">
-              <TypeAnimation
-                cursor={false}
-                sequence={[
-                  'die Polizei',
-                  2000,
-                  'öffentliche Museen',
-                  2000,
-                  'Wohnungsbau',
-                  2000,
-                  'Straßenbeleuchtung',
-                  2000,
-                  'Grundsicherung',
-                  2000,
-                ]}
-                wrapper={'p'}
-                repeat={Infinity}
-              />
-              <span className="text-white"> .</span>
-            </h1>
-            <h1 className="flex">ausgegeben?</h1>
-          </div>
-          <div className="flex-col mt-6 md:mt-16">
-            <iframe
-              title="Beispiel-Visualisierung der Haushaltsdaten"
-              width="400rem"
-              height="369"
-              src="https://haushaltsdaten-staging.odis-berlin.de/share"
-            ></iframe>
-          </div>
-        </div>
-
-        <div className="font-bold text-brand mt-12 md:mt-20 text-xl">
-          <ul>
-            <li>
-              <InternalLink href={'/visualisierung'}>
-                {'→ Alle Daten in der Tree Map erkunden'}
-              </InternalLink>
-            </li>
-          </ul>
-        </div>
-
-        <h2 className="font-bold text-2xl md:text-3xl mt-12 md:mt-20">
-          Schwerpunktthema: Gender Budgeting
+      <div className="lg:w-3/6 m-auto mt-6 md:mt-12 mb-16 md:mb-28">
+        <h2 className="text-2xl md:text-3xl mt-12 md:mt-20">
+          Schwerpunktthemen im aktuellen Haushalt
         </h2>
         <div className="mt-6">
           Berlin ist bis heute eines der wenigen Bundesländer in Deutschland,
           das die Umsetzung von Gender Budgeting im Landeshaushalt explizit
           formuliert hat und inzwischen dabei ist, Gender Budgeting in die
-          normale Haushaltspolitik zu integrieren. Beispiele für die
-          Implementierung von Gender Budget-Ansätzen gibt es in verschiedenen
-          europäischen und außereuropäischen Ländern: Schweiz, Spanien,
-          Österreich, Großbritannien, Australien, Südafrika, Brasilien etc. Die
-          in der Berliner Verwaltung 2003/2004 eingeführte Gender
-          Budget-Nutzenanalyse untersucht überwiegend die Ausgabenseite des
-          Berliner Haushalts, um zu ermitteln, ob die
-          Programme/Angebote/Leistungen vermehrt von Frauen oder Männern in
-          Anspruch genommen werden.
-          <br></br>
-          Auf der Ebene der Hauptverwaltung, also den Senatsverwaltungen,
-          geschieht dies vor allem bei Titeln der Hauptgruppe 6 (Zuweisungen und
-          Zuschüsse ohne Investitionen), hierzu gehören Unterstützungen für
-          natürliche Personen wie auch für Institutionen, z. B. Vereine,
-          Kultureinrichtungen usw. In den Kapiteln der Hauptverwaltung mit
-          Personal werden die Durchschnittsgehälter der Beschäftigten nach
-          Geschlecht ausgewiesen und eventuelle Unterschiede begründet.
+          normale Haushaltspolitik zu integrieren.
           <br></br>
           Auf der Ebene der Bezirksverwaltung werden mittlerweile 116
           Bezirksprodukte unter Gender-Aspekten untersucht, wie beispielsweise
           die Nutzung von Sportflächen, Musikschulunterricht,
           Volkshochschulkurse.
         </div>
-
-        <div className="font-bold text-brand mt-12 md:mt-20 text-xl">
-          <ul>
-            <li className="mb-6 md:mb-20">
-              <InternalLink href={'/schwerpunkte'}>
-                {'→ Ausgewählte Schwerpunktthemen ansehen'}
-              </InternalLink>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
+  </div>
+   
   )
 }
 
