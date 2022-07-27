7e2722f3a088583638dd6696fc99f01627585788
Lisa-Stubert <lisastubert@web.de>
2022-07-27 12:49:31 +0200

    feat(search): extend shown columns in search results

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,11 +5,21 @@ import classNames from 'classnames'
 import { HaushaltsdatenRowType } from '@lib/requests/getRowsByDistrictAndType'
 import { formatCurrency } from '@lib/utils/numberUtil'
 
+interface FilteredSearchResultsType extends HaushaltsdatenRowType {
+  jahr: string
+  titel: string
+  obergruppen_bezeichnung: string
+  hauptgruppen_bezeichnung: string
+  gruppen_bezeichnung: string
+  einzelplan_bezeichnung: string
+  kapitel_bezeichnung: string
+}
+
 const MAX_RENDERED_RESULTS = 500
 
 const toTitleCase = (s: string): string =>
   s.replace(/^_*(.)|_+(.)/g, (_s, c: string, d: string) =>
-    c ? c.toUpperCase() : ' ' + d.toUpperCase()
+    c ? c.toUpperCase() : '-' + d.toUpperCase()
   )
 
 const Table: FC<{ body: React.ReactNode; head: React.ReactNode }> = ({
@@ -32,7 +42,9 @@ export const getStaticProps: GetStaticProps = async () => ({
 })
 
 export const Search: FC = () => {
-  const [results, setResults] = useState<HaushaltsdatenRowType[] | null>(null)
+  const [results, setResults] = useState<FilteredSearchResultsType[] | null>(
+    null
+  )
   const [searchTerm, setSearchTerm] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
 
@@ -63,16 +75,23 @@ export const Search: FC = () => {
       setSearchTerm(value)
     } else {
       setSearchTerm(value)
-      const mappedData: HaushaltsdatenRowType[] = data.map(
-        (row: HaushaltsdatenRowType) => {
+      const mappedData: FilteredSearchResultsType[] = data.map(
+        (row: FilteredSearchResultsType) => {
           return {
             titel_bezeichnung: row['titel_bezeichnung'],
             titel_art: row['titel_art'],
+            jahr: row['jahr'],
             betrag: `${formatCurrency(parseInt(row['betrag'], 10))} €`,
             bereichs_bezeichnung: row['bereichs_bezeichnung'],
-            funktions_bezeichnung: row['funktions_bezeichnung'],
-            oberfunktions_bezeichnung: row['oberfunktions_bezeichnung'],
+            einzelplan_bezeichnung: row['einzelplan_bezeichnung'],
+            kapitel_bezeichnung: row['kapitel_bezeichnung'],
             hauptfunktions_bezeichnung: row['hauptfunktions_bezeichnung'],
+            oberfunktions_bezeichnung: row['oberfunktions_bezeichnung'],
+            funktions_bezeichnung: row['funktions_bezeichnung'],
+            hauptgruppen_bezeichnung: row['hauptgruppen_bezeichnung'],
+            obergruppen_bezeichnung: row['obergruppen_bezeichnung'],
+            gruppen_bezeichnung: row['gruppen_bezeichnung'],
+            titel: row['titel'],
             id: row['id'],
           }
         }
