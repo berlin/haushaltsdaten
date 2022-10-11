381082906893c68462035bd3f955d319a7762818
hhack <hans.f.hack@googlemail.com>
2022-10-11 10:16:01 +0200

    add new banner component file

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+// import React, { useState } from 'react'
+
+export const Banner: FC = () => {
+  //   const [showBanner, setShowBanner] = useState(true)
+  //   const hideBanner = (): void => {
+  //     setShowBanner(false)
+  //   }
+  return <div className=""></div>
+}
