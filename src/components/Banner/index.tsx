b77793c678a9b3fa0e82fd5d1e31c718bbc212fa
hhack <hans.f.hack@googlemail.com>
2022-10-14 10:19:38 +0200

    layout and color

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,14 +6,14 @@ export const Banner: FC = () => {
   const [showBanner, setShowBanner] = useState(true)
 
   return showBanner ? (
-    <div className="bg-brand text-white sticky top-0">
+    <div className="bg-blue text-white">
       <button
         onClick={() => setShowBanner(false)}
         className="p-2 font-medium text-brand cursor-pointer absolute right-0"
       >
         <Cross color1={'white'} />
       </button>
-      <div className="px-4 xl:px-8 py-5 py-8 flex gap-4 items-end flex-wrap">
+      <div className="px-4 sm:px-0 container m-auto py-6 flex gap-4 items-end flex-wrap sm:flex-nowrap">
         <p className="grow">
           <b>Liebe Developer, </b>
           am 23.11.22 um 19 Uhr findet im CityLAB Berlin das nächste Developer
@@ -22,7 +22,7 @@ export const Banner: FC = () => {
           Du bist herzlich willkommen, uns über die Schulter zu schauen und
           selbst aktiv mitzumachen. Wir freuen uns auf euch!
         </p>
-        <span className="shrink-0">
+        <span className="shrink-0 self-center">
           <Button href={'https://www.citylab-berlin.org'}>Mehr Erfahren</Button>
         </span>
       </div>
