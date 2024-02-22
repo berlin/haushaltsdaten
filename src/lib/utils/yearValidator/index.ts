1ec64aef1ac657ae17c20cadd33052589da069a2
ff6347 <hash@ff6347.email>
2024-02-22 15:05:03 +0100

    chore(data): replace data reference to 2024/2025

--- src/lib/utils/yearValidator/index.ts
+++ src/lib/utils/yearValidator/index.ts
@@ -1,6 +1,6 @@
-export const VALID_YEARS = [2022, 2023]
+export const VALID_YEARS = [/*2022, 2023, */ 2024, 2025]
 
-export const DEFAULT_YEAR = 2022
+export const DEFAULT_YEAR = 2024
 
 export const isValidYear = (yearToCheck: number): boolean =>
   VALID_YEARS.includes(yearToCheck)
